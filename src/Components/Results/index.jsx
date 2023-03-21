dbce1725674abac92973030f20dc2fde2c7203b7
Ash Uppal <ashuppal@gmail.com>
2023-03-21 09:57:33 -0700

    change the styling

--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -4,16 +4,9 @@ import './Results.scss';
 
 const Results = props => {
   return (
-    <>
-      <section className="results">
-        <h3>Headers</h3>
-        <pre>{props.headers}</pre>
-        <h3>Count</h3>
-        <pre>{props.count}</pre>
-        <h3>Results</h3>
-        <pre>{props.results}</pre>
-      </section>
-    </>
+    <section>
+      <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+    </section>
   );
 }
 
