12e647918af0b01d3e475f0c470830611bb41971
Ash Uppal <ashuppal@gmail.com>
2023-03-23 18:21:20 -0700

    implemented reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,85 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useReducer } from 'react'
+import axios from 'axios'
 
-import './App.scss';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Form from './Components/Form';
-import Results from './Components/Results';
+import './App.scss'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+import Form from './Components/Form'
+import Results from './Components/Results'
+
+const initialState = {
+  data: null,
+  requestParams: {},
+  loading: false
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_DATA':
+      return { ...state, data: action.payload }
+    case 'SET_REQUESTPARAMS':
+      return { ...state, requestParams: action.payload }
+    case 'SET_LOADING':
+      return { ...state, loading: action.payload }
+    default:
+      return state
+  }
+}
 
 const App = () => {
+  // const [data, setData] = useState(null);
+  // const [requestParams, setRequestParams] = useState({});
+  // const [loading, setLoading] = useState(false);
 
-  const [data, setData] = useState(null);
-  const [requestParams, setRequestParams] = useState({});
-  const [loading, setLoading] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState)
 
-
-  const callApi = (requestParams) => {
+  const callApi = requestParams => {
     // mock output
 
-    const data = {
-      count: 2,
-      results: [
-        { name: 'fake thing 1', url: 'http://fakethings.com/1' },
-        { name: 'fake thing 2', url: 'http://fakethings.com/2' },
-      ],
-    };
-    setData(data);
-    setRequestParams(requestParams);
+    // const data = {
+    //   count: 2,
+    //   results: [
+    //     { name: 'fake thing 1', url: 'http://fakethings.com/1' },
+    //     { name: 'fake thing 2', url: 'http://fakethings.com/2' },
+    //   ],
+    // };
+    //dispatch({type: 'SET_DATA',payload:data});
+    dispatch({ type: 'SET_REQUESTPARAMS', payload: requestParams })
   }
 
   useEffect(() => {
     const getData = async () => {
-      if(requestParams.method && requestParams.url){
-        setLoading(true);
-        const response = await axios(requestParams);
-        const data = response.data;
-        setData(data);
-        setLoading(false);
+      if (state.requestParams.method && state.requestParams.url) {
+        dispatch({ type: 'SET_LOADING', payload: true })
+        const response = await axios(state.requestParams)
+        const data = response.data
+        // setData(data);
+        // setLoading(false);
+        dispatch({ type: 'SET_DATA', payload: data })
+        // dispatch({type:'SET_REQUESTPARAMS', payload:requestParams})
+        dispatch({ type: 'SET_LOADING', payload: false })
       }
     }
-    getData();
-  }, [requestParams]);
+    getData()
+  }, [state.requestParams])
 
   return (
     <>
       <Header />
-      <div>Request Method: {requestParams.method ? requestParams.method.toUpperCase() : ''}</div>
-      <div>URL: {requestParams.url}</div>
-      <div>Data: {requestParams.data}</div>
+      <div>
+        Request Method:{' '}
+        {state.requestParams.method
+          ? state.requestParams.method.toUpperCase()
+          : ''}
+      </div>
+      <div>URL: {state.requestParams.url}</div>
+      <div>Data: {state.requestParams.data}</div>
 
       <Form handleApiCall={callApi} />
-      <Results data={data} loading={loading} />
+      <Results data={state.data} loading={state.loading} />
       <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,54 +1,79 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import './Form.scss';
+
+export const intialState = {
+  method : 'get',
+  url : '',
+  data: '',
+}
+
+export const formReducer = (state = intialState,action) =>{
+  switch(action.type){
+    case 'SET_METHOD':
+      return {...state, method: action.payload};
+    case 'SET_URL': 
+    return  {...state, url: action.payload};
+    case 'SET_DATA':
+      return {...state, data: action.payload};
+      default:
+        return state;
+  }
+}
+
+
+
 
 const Form = (props) => {
 
-  const [method, setMethod] = useState('get');
-  const [url, setUrl] = useState('');
-  const [data, setData] = useState('');
-
+const [state,dispatch] = useReducer(formReducer, intialState);
 
   const handleSubmit = e => {
     e.preventDefault();
     const formData = {
-      method,
-      url,
-      data,
+      method : state.method,
+      url: state.url,
+      data: state.data,
     };
     props.handleApiCall(formData);
   }
+
+  const changeUrl = (e) => dispatch({type:'SET_URL',payload: e.target.value});
 
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label >
           <span>URL: </span>
-          <input data-testid="url-input" name='url' type='text' onChange={(e) => setUrl(e.target.value)}/>
+          <input
+           data-testid="url-input" 
+           name='url' type='text' 
+           onChange={changeUrl}/>
           <button data-testid="button" type="submit">GO!</button>
         </label>
         <label>json data (if necessary)
-          <textarea rows="4" cols="50" onChange={(e) => setData(e.target.value)}/>
+          <textarea rows="4" cols="50"
+           onChange={(e) => dispatch({type:'SET_DATA',payload:e.target.value})}/>
         </label>
         <label className="methods">
           <span
             data-testid="get-span"
-            onClick={(e) => setMethod(e.target.id)}
-            style={{ backgroundColor: method === 'get' ? 'gray' : '#ccc' }} id="get"
+            onClick={(e) => dispatch({type:'SET_METHOD',payload: e.target.id})}
+            style={{ backgroundColor: state.method === 'get' ? 'gray' : '#ccc' }} id="get"
           >
             GET
           </span>
           <span
             data-testid="post-span"
-            onClick={(e) => setMethod(e.target.id)}
-            style={{ backgroundColor: method === 'post' ? 'gray' : '#ccc' }} id="post"
+            onClick={(e) => dispatch({type:'SET_METHOD',payload: e.target.id})}
+            style={{ backgroundColor: state.method === 'post' ? 'gray' : '#ccc' }} id="post"
           >POST</span>
           <span
-            onClick={(e) => setMethod(e.target.id)}
-            style={{ backgroundColor: method === 'put' ? 'gray' : '#ccc' }} id="put"
+            onClick={(e) => dispatch({type:'SET_METHOD',payload: e.target.id})}
+            style={{ backgroundColor: state.method === 'put' ? 'gray' : '#ccc' }} id="put"
           >PUT</span>
           <span
-            onClick={(e) => setMethod(e.target.id)}
-            style={{ backgroundColor: method === 'delete' ? 'gray' : '#ccc' }} id="delete"
+            onClick={(e) => dispatch({type:'SET_METHOD',payload: e.target.id})}
+            style={{ backgroundColor:state.method === 'delete' ? 'gray' : '#ccc' }} id="delete"
           >DELETE</span>
         </label>
       </form>
